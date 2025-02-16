3c1946475f377fbd3eab265295535b8d6e2f38f9
JangG1 <jangjiwon0417@naver.com>
2025-02-16 19:14:36 +0900

    Back Test in Cloud

--- Clush_Back_SpringBoot/src/main/java/com/Clush/app/Domain/Board.java
+++ Clush_Back_SpringBoot/src/main/java/com/Clush/app/Domain/Board.java
@@ -1,24 +1,20 @@
 package com.Clush.app.Domain;
 
 import java.sql.Timestamp;
-
 import java.util.ArrayList;
 import java.util.List;
 
-//import javax.persistence.*; [2025/02/07] 해당 import로 에러 발생 (org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'controller' defined in file) 
 import jakarta.persistence.*;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 @JsonIgnoreProperties(ignoreUnknown = true)  // JSON에 없는 필드는 무시
 @Entity
@@ -49,9 +45,13 @@ public class Board {
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference  // 무한 참조 방지
     private List<Comment> comments = new ArrayList<>();
-    
-    public void setBoardNo(int boardNo) { //(Long도 받을 수 있도록 오버로딩 가능)
+
+    public void setBoardNo(int boardNo) {
         this.boardNo = boardNo;
     }
-}
 
+    // 수동으로 builder 메서드 추가 (Optional)
+    public static BoardBuilder builderWithBoardNo(int boardNo) {
+        return builder().boardNo(boardNo);  // builder로 생성하며 boardNo만 설정
+    }
+}
--- Clush_Back_SpringBoot/src/main/java/com/Clush/app/Domain/Comment.java
+++ Clush_Back_SpringBoot/src/main/java/com/Clush/app/Domain/Comment.java
@@ -2,9 +2,8 @@ package com.Clush.app.Domain;
 
 import java.sql.Timestamp;
 
-import org.hibernate.annotations.CreationTimestamp;
-
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -12,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -37,17 +37,15 @@ public class Comment {
     @CreationTimestamp
     private Timestamp comment_date;
 
- // JSON에서 "board": "1"로 들어오는 값을 Board 객체로 변환
+    // JSON에서 "board": "1"로 들어오는 값을 Board 객체로 변환
     public void setBoard(Long boardNo) {
         Board board = new Board(); // 새 Board 객체 생성
         board.setBoardNo(boardNo.intValue()); // int로 변환 후 설정
         this.board = board;
     }
-    
-    
-    // 새롭게 추가할 Board 객체를 받는 setter
-    public void setBoard(Board board) {
-        this.board = board;
+
+    // 수동으로 builder 메서드 추가 (Optional)
+    public static CommentBuilder builderWithCommentNo(int commentNo) {
+        return builder().commentNo(commentNo);  // builder로 생성하며 commentNo만 설정
     }
 }
-
